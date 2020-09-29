676db9af63d2ee19b863b843f2e15883b3bb41e6
OlegBirykov <p_petro75@mail.ru>
2020-09-29 20:43:09 +0300

    Выполнено задание №1

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,3 @@
-import Popover from './Popover';
-
-export default function main() {
-  const button = document.querySelector('.btn');
-  let popover;
-
-  button.addEventListener('click', () => {
-    if (!button.dataset.tooltipVisible) {
-      popover = new Popover(button);
-      button.dataset.tooltipVisible = 'yes';
-    } else {
-      popover.delete();
-      button.dataset.tooltipVisible = '';
-    }
-  });
-}
+import main from './main';
 
 main();
